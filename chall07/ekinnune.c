4307192f7b2f885ce92501a2b2c2a93365db9a60
U-DESKTOP-881U9VF\MAGPUL <eljas.kinnunen98@gmail.com>
2020-05-03 19:17:06 +0300

    smoll progress

--- chall07/ekinnune.c
+++ chall07/ekinnune.c
@@ -13,42 +13,45 @@ int	ft_count_width(char *str)
 	return (i);
 }
 
-char	**ft_allocate(int w, int h)
+char	**ft_allocate(int w, int h, char **argv)
 {
+	int i;
+	int j;
 	char **shape;
 
-	shape = (char **)malloc(sizeof(char *) * h);
-	while (h >= 0)
-	{
-		shape[h] = (char *)malloc(sizeof(char)* w);
-		h--;
-	}
-	return (shape);
-/*	int j;
-	int k;
+	i = 0;
 	j = 0;
-	k = 0;
-
-	while (i <= (w * h))
+	shape = (char **)malloc(sizeof(char *)* h);
+	while (i < h)
 	{
-		if (av[j][k] == '\0')
-		{
-			j++;
-			k = 0;
-		}
-		shape[i] = &av[j][k];
-		k++;
+		shape[i] = (char *)malloc(sizeof(char)* (w + 1));
 		i++;
 	}
-*/
+	i = 0;
+	while (j < h)
+	{
+		shape[j][i] = argv[j + 1][i];
+		i++;
+		if(argv[j + 1][i] == '\0')
+		{
+			shape[j][i] = '\0';
+			j++;
+			i = 0;
+		}
+	}
+	return (shape);
 }
+
+//	ft_draw_hollow(int widt, int heig, char *list, char **box)
 
 char	*ft_make_list(int widt, int heig, char **box)
 {
-	int size;
 	int i;
 	int h;
 	int w;
+	int pattern_w;
+	int pattern_h;
+
 //	int temp_w;
 //	int temp_h;
 	char *list;
@@ -56,41 +59,45 @@ char	*ft_make_list(int widt, int heig, char **box)
 	w = 0;
 	h = 0;
 	i = 0;
+	pattern_w = widt;
+	pattern_h = heig;
 //	temp_w = widt;
 //	temp_h = heig;
-	size = heig * widt;
-	list = (char *)malloc(sizeof(char)* size);
-	while (i <= size)
+	list = (char *)malloc(sizeof(char)* (heig * widt));
+
+	while (pattern_w > 0 || pattern_h > 0)
 	{
-		while (box[h][w] != '\0')
-		{
-			list[i] = box[h][w];
-			w++;
-			i++;
-		}
-		h++;
-		i++;
+		if (w > 0)
+			while (w < pattern_w)
+			{
+				list[i] = box[h][w];
+				w++;
+				i++;
+			}
+		
+	pattern_w -= 2;
+	pattern_h -= 2;
 	}
 
 	return (list);
 }
 
-int	main(int ac, char *av[])
+int	main(int ac, char **av)
 {
 	int widt;
 	int heig;
-	char *list;
+//	char *list;
 	char **shape;
 	heig = ac - 1;
 	if (ac > 1)
 	{
 		widt = ft_count_width(av[1]);
 		printf("length = %d height = %d", widt, heig);
-		shape = ft_allocate(widt, heig);
+		shape = ft_allocate(widt, heig, av);
 		printf("%s %s", shape[0], shape[1]);
 
-		list = ft_make_list(widt, heig, av);
-		printf("[%s]", list);
+//		list = ft_make_list(widt, heig, shape);
+//		printf("[%s]", list);
 	}
 	return (0);
 }
